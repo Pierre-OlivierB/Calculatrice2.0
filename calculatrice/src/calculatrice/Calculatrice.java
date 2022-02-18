b762916e26d9fc58927e0a603a10527813d8a98d
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-02-18 14:31:43 +0100

    initialisation calculatrice

---
+++ calculatrice/src/calculatrice/Calculatrice.java
@@ -0,0 +1,43 @@
+package calculatrice;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Calculatrice {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculatrice window = new Calculatrice();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Calculatrice() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+
+}
