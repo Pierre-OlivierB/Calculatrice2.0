877e904959d333beadd1871ea6e05bf0d2e421d5
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-02-18 15:38:25 +0100

    ajout d'un bouton 1 qui agit sur l'écran

--- calculatrice/src/calculatrice/Calculatrice.java
+++ calculatrice/src/calculatrice/Calculatrice.java
@@ -1,8 +1,16 @@
 package calculatrice;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Desktop.Action;
+import java.awt.event.ActionEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,12 +23,13 @@ public class Calculatrice extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 3900224470578951048L;
-	private JFrame frame;
+	//private JFrame frame;
 	private JPanel container = new JPanel();
 	
 	private JLabel screen = new JLabel();
 	
-	
+	private JButton btn_one=new JButton("1");
+	private JButton btn_two=new JButton("2");
 	
 
 	/**
@@ -48,6 +57,7 @@ public class Calculatrice extends JFrame {
 		this.setTitle("Calculatrice");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+		container.setLayout(new FlowLayout());
 		initialize();
 		this.setContentPane(container);
 	}
@@ -61,8 +71,23 @@ public class Calculatrice extends JFrame {
 		screen.setFont(policeFont);
 		
 		screen.setHorizontalAlignment(JLabel.RIGHT);
+		screen.setPreferredSize(new Dimension(100,30));
 		
-
+		JPanel panScreen=new JPanel();
+		panScreen.setPreferredSize(new Dimension(100,30));
+		//ajout boutton
+		
+		btn_one.addActionListener((e)->OneListener(e));
+		container.add(btn_one,BorderLayout.EAST);
+		
+		//ajout ecran
+		panScreen.add(screen);
+		panScreen.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		container.add(panScreen,BorderLayout.NORTH);
+	}
+	private void OneListener(ActionEvent e) {
+		screen.setText("1");
 	}
 
 }
